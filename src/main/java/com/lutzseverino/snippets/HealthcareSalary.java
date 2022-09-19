2c184e6f381ebfd8aefeb34bd371f4269184dae3
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-09-19 10:31:21 +0200

    feat: add respective pseudocode

--- src/main/java/com/lutzseverino/snippets/HealthcareSalary.java
+++ src/main/java/com/lutzseverino/snippets/HealthcareSalary.java
@@ -6,6 +6,39 @@ import java.util.Scanner;
 
 @Name("overtime") @Name("salary")
 public class HealthcareSalary implements Snippet {
+
+    /*
+        START
+            SHOW ("¿Estás enfermo?" (s/n)")
+            READ (isSick)
+
+            SHOW ("¿Cuántas horas extra has trabajado?")
+            READ (overtimeHours)
+
+            baseSalary <- 1250
+            frstOvertime <- 15
+            secondOvertime <- 12
+            overtimeSalary <- 0
+
+            IF (isSick) THEN
+                baseSalary <- baseSalary + 250
+                firstOvertime <- firstOvertime + 5
+                secondOvertime <- secondOvertime +5
+            END IF
+
+            IF (overtimeHours > 0) THEN
+                IF (overtimeHours <= 5) THEN
+                    overtimeSalary <- overtimeHours * firstOvertime
+                ELSE THEN
+                    overtimeSalary <- (5 * firstOvertime) + ((overtimeHours - 5) * secondOvertime)
+                END IF
+            END IF
+
+            totalSalary <- baseSalary + overtimeSalary
+
+            SHOW ("Tus horas extras constituyen una ganancia de €" + overtimeSalary);
+            SHOW ("Lo que deja un salario final de €" + totalSalary);
+     */
     @Override public void run() {
         Scanner scanner = new Scanner(System.in);
 
