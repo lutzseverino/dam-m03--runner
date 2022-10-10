38c5c14517fc40eefaa97d2ace7729643dade709
jasperls <jasperlutzseverino@gmail.com>
2022-10-10 13:57:20 +0200

    feat: create question

---
+++ src/main/java/com/lutzseverino/question/Question.java
@@ -0,0 +1,81 @@
+package com.lutzseverino.question;
+
+import com.lutzseverino.util.ValidationUtils;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Question {
+    private final String errorMessage;
+    private final Scanner scanner;
+
+    public Question(String errorMessage) {
+        this.errorMessage = errorMessage;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public Question() {
+        this("Por favor, ingresa una respuesta válida.");
+    }
+
+    public String ask(String question, Function<String, Boolean> validator) {
+        String answer;
+
+        System.out.print(question);
+        answer = scanner.nextLine();
+
+        while (!validator.apply(answer)) {
+            System.out.print(errorMessage + "\n" + question);
+            answer = scanner.nextLine();
+        }
+
+        return answer;
+    }
+
+    public String ask(String question) {
+        return ask(question, String::isBlank);
+    }
+
+    public int askInt(String question, Function<Integer, Boolean> validator) {
+        return Integer.parseInt(
+                ask(question, s -> {
+                    if (!ValidationUtils.isInt(s)) return false;
+                    return validator.apply(Integer.parseInt(s));
+                })
+        );
+    }
+
+    public int askInt(String question) {
+        return askInt(question, i -> true);
+    }
+
+    public double askDouble(String question, Function<Double, Boolean> validator) {
+        return Double.parseDouble(
+                ask(question, s -> {
+                    if (!ValidationUtils.isDouble(s)) return false;
+                    return validator.apply(Double.parseDouble(s));
+                })
+        );
+    }
+
+    public double askDouble(String question) {
+        return askDouble(question, d -> true);
+    }
+
+    public boolean askBoolean(String question, Function<Boolean, Boolean> validator) {
+        return Boolean.parseBoolean(
+                ask(question, s -> {
+                    if (!ValidationUtils.isBoolean(s)) return false;
+                    return validator.apply(Boolean.parseBoolean(s));
+                })
+        );
+    }
+
+    public boolean askBoolean(String question) {
+        return askBoolean(question, b -> true);
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
